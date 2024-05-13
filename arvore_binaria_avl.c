2d0ebf2479f17a8db6e40c04295cbe3400998c72
Alexandre <alexandrevilela.sds@gmail.com>
2024-05-13 10:33:12 -0300

    feature: correção de erros e alteração na função remover

--- arvore_binaria_avl.c
+++ arvore_binaria_avl.c
@@ -66,6 +66,8 @@ No* rotacaoEsquerda(No* r){
 
     r->altura = maior(alturaDoNo(r->esquerda), alturaDoNo(r->direita)) + 1;
     y->altura = maior(alturaDoNo(y->esquerda), alturaDoNo(y->direita)) + 1;
+
+    return y;
 }
 
 // função para a rotação a direita
@@ -99,32 +101,6 @@ No* rotacaoEsquerdaDireita(No* r){
     return rotacaoDireita(r);
 }
 
-/*
-Insere um novo nó na árvore
-raiz -> raiz da árvore
-x -> valor a ser inserido
-retorna: endereço do novo nó ou nova raiz após o balanceamento
-*/
-No* inserir(No* raiz, int x){
-    if(raiz == NULL) // árvore vazia
-        return novoNo(x);
-    else{ // inserção será à esquerda ou à direita
-        if(x < raiz->valor)
-            raiz->esquerda = inserir(raiz->esquerda, x);
-        else if(x > raiz->direita)
-            raiz->direita = inserir(raiz->direita, x);
-        else
-            printf("\nInserção não realizada!\nO elemento %d já existe!\n", x);
-    }
-
-    //Recalcular a altura de todos os nós entre a raiz e o novo nó inserido
-    raiz->altura = maior(alturaDoNo(raiz->esquerda), alturaDoNo(raiz->direita)) + 1;
-
-    //verifica a necessidade rebalancear a árvore
-    raiz = balancear(raiz);
-
-    return raiz;
-}
 
 /*Função para realizar o balanceamento da árvore aṕos uma inserção ou remoção
 Recebe o nó que está desbalanceado e retorna a nova raiz após o balanceamento*/
@@ -151,54 +127,73 @@ No* balancear(No* raiz){
     return raiz;
 }
 
-No* remover(No* raiz, int chave){
-    if(raiz == NULL){
+/*
+Insere um novo nó na árvore
+raiz -> raiz da árvore
+x -> valor a ser inserido
+retorna: endereço do novo nó ou nova raiz após o balanceamento
+*/
+No* inserir(No* raiz, int x){
+    if(raiz == NULL) // árvore vazia
+        return novoNo(x);
+    else{ // inserção será à esquerda ou à direita
+        if(x < raiz->valor)
+            raiz->esquerda = inserir(raiz->esquerda, x);
+        else if(x > raiz->valor)
+            raiz->direita = inserir(raiz->direita, x);
+        else
+            printf("\nInserção não realizada!\nO elemento %d já existe!\n", x);
+    }
+
+    //Recalcular a altura de todos os nós entre a raiz e o novo nó inserido
+    raiz->altura = maior(alturaDoNo(raiz->esquerda), alturaDoNo(raiz->direita)) + 1;
+
+    //verifica a necessidade rebalancear a árvore
+    raiz = balancear(raiz);
+
+    return raiz;
+}
+
+
+
+No* remover(No* raiz, int chave) {
+    if (raiz == NULL) {
         printf("Valor não encontrado!\n");
         return NULL;
-    }else{ // Procura o nó a remover
-        if(raiz->valor == chave){
-            // remove nós folhas (nós sem filhos)
-            if(raiz->esquerda == NULL && raiz->direita == NULL){
+    }
+
+    if (chave < raiz->valor)
+        raiz->esquerda = remover(raiz->esquerda, chave);
+    else if (chave > raiz->valor)
+        raiz->direita = remover(raiz->direita, chave);
+    else {
+        if (raiz->esquerda == NULL || raiz->direita == NULL) {
+            No* aux = raiz->esquerda ? raiz->esquerda : raiz->direita;
+            if (aux == NULL) {
                 free(raiz);
                 printf("Elemento folha removido: %d!\n", chave);
-
-            }else{
-                if(raiz->esquerda != NULL && raiz->direita != NULL){
-                    No* aux = raiz->esquerda;
-                    while(aux->direita != NULL)
-                        aux = aux->direita;
-
-                    raiz->valor = aux->valor;
-                    aux->valor = chave;
-                    printf("Elmento trocado: %d!\n", chave);
-                    raiz->esquerda = remover(raiz->esquerda, chave);
-                    return raiz;
-                }else{
-                    //remover nós que possuem apenas 1 filho
-                    No* aux;
-                    if(raiz->esquerda != NULL)
-                        aux = raiz->esquerda;
-                    else
-                        aux = raiz->direita;
-                    free(raiz);
-                    printf("Elemento com 1 filho removido: %d!\n", chave);
-                    return aux;
-                }
+                return NULL;
+            } else {
+                *raiz = *aux;
+                free(aux);
+                printf("Elemento com 1 filho removido: %d!\n", chave);
+                return raiz;
             }
-        }else{
-            if(chave < raiz->esquerda)
-                remover(raiz->esquerda, chave);
-            else
-                remover(raiz->direita, chave);
-        }
-        // Recalculae a altura de todos os nós entre a raiz e o novo nó inserido
-        raiz->altura = maior(alturaDoNo(raiz->esquerda), alturaDoNo(raiz->direita)) + 1;
+        } else {
+            No* aux = raiz->esquerda;
+            while (aux->direita != NULL)
+                aux = aux->direita;
 
-        //verifica a necessidade de rebalancear a árvore
-        raiz = balancear(raiz);
-        
-        return raiz;
+            raiz->valor = aux->valor;
+            raiz->esquerda = remover(raiz->esquerda, aux->valor);
+        }
     }
+
+    raiz->altura = maior(alturaDoNo(raiz->esquerda), alturaDoNo(raiz->direita)) + 1;
+
+    raiz = balancear(raiz);
+
+    return raiz;
 }
 
 void imprimir(No* raiz,int nivel){
