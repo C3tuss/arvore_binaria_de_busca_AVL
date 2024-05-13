bbe30be1b298296bff02a109a2120b1fdfb55eb2
Alexandre <alexandrevilela.sds@gmail.com>
2024-05-13 10:09:33 -0300

    Feature: implementação na main

--- arvore_binaria_avl.c
+++ arvore_binaria_avl.c
@@ -217,6 +217,36 @@ void imprimir(No* raiz,int nivel){
 
 int main(){
 
+    int opcao, valor;
+    No* raiz = NULL;
+
+    do{
+        printf("\n\n\t0 - Sair\n\t1 - Inserir\n\t2 - Remover\n\t3 - Imprimir\n\n");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 0:
+            printf("Saindo!!!");
+            break;
+        case 1:
+            printf("\tDigite o valor a ser inserido: ");
+            scanf("%d", &valor);
+            raiz = inserir(raiz, valor);
+            break;
+        case 2:
+            printf("\tDigite o valor a ser removido: ");
+            scanf("%d", &valor);
+            raiz = remover(raiz, valor);
+            break;
+        case 3:
+            imprimir(raiz, 1);
+            break;
+        default:
+            printf("\nOpção invalida!!!\n");
+        }
+
+    }while(opcao != 0);
 
     return 0;
 }
